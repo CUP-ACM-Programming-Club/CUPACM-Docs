98ca6e0e994cee1ec98a6aab8bab9c88a3bb086b
Ryan Lee <gxlhybh@gmail.com>
2019-09-08 12:25:09 +0800

    Init docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    base: '/CUPACM-Docs',
+    title: "CUPACM Documents",
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        sidebar: 'auto'
+    }
+};
