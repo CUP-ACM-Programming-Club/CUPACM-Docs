e5a95f45f8b484971785f5443027263c8375d1ca
Ryan Lee <gxlhybh@gmail.com>
2019-09-11 14:28:38 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,9 @@ module.exports = {
     base: '/',
     title: "CUPACM Documents",
     head: [
-        ['link', { rel: 'stylesheet', href: 'https://shadow.elemecdn.com/npm/katex@0.11.0/dist/katex.min.css' }]
+        ['link', {rel: 'stylesheet', href: 'https://shadow.elemecdn.com/npm/katex@0.11.0/dist/katex.min.css'}]
     ],
+    plugins: ['@vuepress/back-to-top'],
     markdown: {
         lineNumbers: true,
         plugins: ['@ryanlee2014/katex']
@@ -15,34 +16,38 @@ module.exports = {
         docsDir: 'docs',
         docsBranch: 'master',
         editLinkText: '在Github上编辑此页面',
-        sidebar: {
-            '/code_template/': loadLocaleMessages('code_template'),
-            '/solution/': loadLocaleMessages('solution'),
-            '/contribute/': loadLocaleMessages('contribute')
-        },
+        sidebar: loadSidebarContents(),
         lastUpdated: '上次更新',
         nav: [
             {
                 text: '首页', link: '/'
             },
             {
-                text: '模板库', link: '/code_template/index'
+                text: '模板库', link: '/code_template/'
             },
             {
-                text: '题解', link: '/solution/index'
+                text: '题解', link: '/solution/'
             },
             {
-                text: '构建本文档', link: '/contribute/index'
+                text: '构建本文档', link: '/contribute/'
             }
         ]
     }
 };
 
-function loadLocaleMessages(dir) {
+function loadSidebarContents() {
+    const sidebarMap = {};
+    glob.sync(`docs/*`)
+        .map(dir => dir.replace('docs', '').replace('/', ''))
+        .forEach(dir => sidebarMap[`/${dir}/`] = loadDirContents(dir));
+    return sidebarMap
+}
+
+function loadDirContents(dir) {
     return ['', ...glob.sync(`docs/${dir}/*.md`)
         .map(f => f.replace('docs/', '')
             .replace(dir + '/', '')
             .replace('\.md', ''))
         .filter(e => e !== 'index')
-            ];
+    ];
 }
