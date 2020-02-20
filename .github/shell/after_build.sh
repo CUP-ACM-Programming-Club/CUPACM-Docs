db7750946a628affff66820499b1fe00fa3b5f53
Ryan Lee <gxlhybh@gmail.com>
2020-02-20 19:28:42 +0800

    move script order

---
+++ .github/shell/after_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+WEBPATH="https://cdn.jsdelivr.net/gh/CUP-ACM-Programming-Club/CUPACM-Docs-CDN@v$(cat VERSION)/"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+sed -i "" "s|a.p=\"/\"|a.p=\"$WEBPATH\"|g" docs/.vuepress/dist/assets/js/app.*.js
+else
+sed -i  "s|a.p=\"/\"|a.p=\"$WEBPATH\"|g" docs/.vuepress/dist/assets/js/app.*.js
+fi
+node .github/shell/build_cdn_environment.js
