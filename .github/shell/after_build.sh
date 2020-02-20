5dfae0abe533cfe990b3dce7c17dd9cb39a7e251
Ryan Lee <gxlhybh@gmail.com>
2020-02-20 19:38:43 +0800

    Fix css url problem

--- .github/shell/after_build.sh
+++ .github/shell/after_build.sh
@@ -2,7 +2,9 @@
 WEBPATH="https://cdn.jsdelivr.net/gh/CUP-ACM-Programming-Club/CUPACM-Docs-CDN@v$(cat VERSION)/"
 if [[ "$OSTYPE" == "darwin"* ]]; then
 sed -i "" "s|a.p=\"/\"|a.p=\"$WEBPATH\"|g" docs/.vuepress/dist/assets/js/app.*.js
+sed -i "" "s|assets/img/|$WEBPATH/assets/img/|g" docs/.vuepress/dist/assets/css/*.css
 else
 sed -i  "s|a.p=\"/\"|a.p=\"$WEBPATH\"|g" docs/.vuepress/dist/assets/js/app.*.js
+sed -i  "s|assets/img/|$WEBPATH/assets/img/|g" docs/.vuepress/dist/assets/css/*.css
 fi
 node .github/shell/build_cdn_environment.js
